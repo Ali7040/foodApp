2a55b6f708c4105f96a02e53e75304297b54ae93
Ali7040 <ha4219466@gmail.com>
2024-02-11 13:05:45 +0500

    update

--- src/app/component/pages/food-page/food-page.component.ts
+++ src/app/component/pages/food-page/food-page.component.ts
@@ -1,25 +1,36 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FoodService } from '../../../services/food.service';
 import { CommonModule } from '@angular/common';
 import { StarRatingComponent } from '../../star-rating/star-rating.component';
+import { TagsComponent } from '../../tags/tags.component';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
   standalone: true,
-  imports: [CommonModule, StarRatingComponent],
+  imports: [CommonModule, StarRatingComponent, TagsComponent, RouterLink],
   templateUrl: './food-page.component.html',
   styleUrl: './food-page.component.scss',
 })
 export class FoodPageComponent {
   food!: Food;
 
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
       if (params['id']) {
         this.food = foodService.getFoodById(params['id']);
       }
     });
+  }
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 }
