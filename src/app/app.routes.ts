8bbbc6c7b520841d1daa6956b0d6a14f7af47f22
Ali7040 <ha4219466@gmail.com>
2024-02-12 13:21:48 +0500

    frontend updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/pages/home/home.component';
 import { FoodPageComponent } from './component/pages/food-page/food-page.component';
+import { CartPageComponent } from './component/pages/cart-page/cart-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'food/:id',
     component: FoodPageComponent,
+  },
+  {
+    path: 'cart-page',
+    component: CartPageComponent,
   },
 ];
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  cartQuantity = 0;
+
+  constructor(cartService: CartService) {
+    // to inject the service
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    });
+  }
+}
