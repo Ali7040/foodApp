6e7efa00de90b4fba9f95a0bd3e45e179e15bfb1
Ali7040 <ha4219466@gmail.com>
2024-02-14 19:19:33 +0500

    add title component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/pages/home/home.component';
 import { FoodPageComponent } from './component/pages/food-page/food-page.component';
 import { CartPageComponent } from './component/pages/cart-page/cart-page.component';
+import { LoginPageComponent } from './component/pages/login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'cart-page',
     component: CartPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
   },
 ];
