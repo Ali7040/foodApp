e421cc4305901dce5f795ecc81a43188782dbe00
Ali7040 <ha4219466@gmail.com>
2024-02-15 18:35:12 +0500

    update tags component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { Toast } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient()],
