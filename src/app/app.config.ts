78331856455b51ee82448f463ecc8647384e3334
Ali7040 <ha4219466@gmail.com>
2024-02-16 19:41:04 +0500

    update Header component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { Toast } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient()],
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +14,22 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   cartQuantity = 0;
+  user!: User;
 
-  constructor(cartService: CartService) {
+  constructor(cartService: CartService, private userService: UserService) {
     // to inject the service
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
     });
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    });
+  }
+  logout() {
+    this.userService.logout();
+  }
+
+  get isAuth() {
+    return this.user.token;
   }
 }
