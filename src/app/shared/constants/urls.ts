2d7e199412333486ead62b1e715c50882f8e4d6b
Ali7040 <ha4219466@gmail.com>
2024-02-22 17:43:59 +0500

    update files and add urls and IUser type

---
+++ src/app/shared/constants/urls.ts
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://localhost:3000';
+
+export const FOODS_URL = BASE_URL + '/api/foods';
+export const FOODS_TAGS_URL = FOODS_URL + '/tags';
+export const FOODS_BY_SEARCH_URL = FOODS_URL + '/search/';
+export const FOODS_BY_TAG_URL = FOODS_URL + '/tag/';
+export const FOODS_BY_ID_URL = FOODS_URL + '/';
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
